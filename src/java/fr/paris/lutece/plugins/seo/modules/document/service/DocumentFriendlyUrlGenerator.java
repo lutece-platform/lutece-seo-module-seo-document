733f5bd9db86fc7d140cfa4112e698b80950d4ad
pierrelevy <pierrelevy@users.noreply.github.com>
2012-07-19 23:41:56 +0000

    SEO document module - work in progress - generator improvments
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/seo/module-seo-document@42405 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/seo/modules/document/service/DocumentFriendlyUrlGenerator.java
+++ src/java/fr/paris/lutece/plugins/seo/modules/document/service/DocumentFriendlyUrlGenerator.java
@@ -33,12 +33,17 @@
  */
 package fr.paris.lutece.plugins.seo.modules.document.service;
 
+import fr.paris.lutece.plugins.seo.service.generator.GeneratorOptions;
+import fr.paris.lutece.plugins.seo.service.generator.FriendlyUrlGenerator;
 import fr.paris.lutece.plugins.document.business.Document;
 import fr.paris.lutece.plugins.document.business.DocumentHome;
 import fr.paris.lutece.plugins.document.business.publication.DocumentPublication;
 import fr.paris.lutece.plugins.document.business.publication.DocumentPublicationHome;
 import fr.paris.lutece.plugins.seo.business.FriendlyUrl;
-import fr.paris.lutece.plugins.seo.service.*;
+import fr.paris.lutece.plugins.seo.service.FriendlyUrlUtils;
+import fr.paris.lutece.plugins.seo.service.SEODataKeys;
+import fr.paris.lutece.plugins.seo.service.sitemap.SitemapUtils;
+import fr.paris.lutece.portal.service.datastore.DatastoreService;
 
 import java.sql.Date;
 
@@ -53,27 +58,42 @@ import java.util.List;
  */
 public class DocumentFriendlyUrlGenerator implements FriendlyUrlGenerator
 {
-    private static final String GENERATOR_NAME = "Document Alias Generator";
+    private static final String GENERATOR_NAME = "Document Friendly URL Generator";
     private static final String TECHNICAL_URL = "/jsp/site/Portal.jsp?document_id={0}&portlet_id={1}";
     private static final String SLASH = "/";
 
+    private static final String DEFAULT_CHANGE_FREQ = SitemapUtils.CHANGE_FREQ_VALUES[3];
+    private static final String DEFAULT_PRIORITY = SitemapUtils.PRIORITY_VALUES[3];
+
+    private boolean _bCanonical;
+    private boolean _bSitemap;
+    private String _strChangeFreq;
+    private String _strPriority;
+    
     @Override
     public String generate( List<FriendlyUrl> list, GeneratorOptions options )
     {
         int nStatus = DocumentPublication.STATUS_PUBLISHED;
         Date date = new Date( 0 );
         Collection<DocumentPublication> pub = DocumentPublicationHome.findSinceDatePublishingAndStatus( date, nStatus );
+        
+        init();
 
         for ( DocumentPublication p : pub )
         {
             Document document = DocumentHome.findByPrimaryKey( p.getDocumentId(  ) );
-            FriendlyUrl rule = new FriendlyUrl(  );
-            rule.setFriendlyUrl( SLASH + FriendlyUrlUtils.convertToFriendlyUrl( document.getTitle(  ) ) );
+            FriendlyUrl url = new FriendlyUrl(  );
+            url.setFriendlyUrl( SLASH + FriendlyUrlUtils.convertToFriendlyUrl( document.getTitle(  ) ) );
 
             Object[] args = { p.getDocumentId(  ), p.getPortletId(  ) };
             String strTechnicalUrl = MessageFormat.format( TECHNICAL_URL, args );
-            rule.setTechnicalUrl( strTechnicalUrl );
-            list.add( rule );
+            url.setTechnicalUrl( strTechnicalUrl );
+            url.setCanonical( _bCanonical );
+            url.setSitemap( _bSitemap );
+            url.setSitemapChangeFreq(_strChangeFreq );
+            url.setSitemapLastmod( SitemapUtils.formatDate( document.getDateModification() ));
+            url.setSitemapPriority( _strPriority );
+            list.add( url );
         }
 
         return "";
@@ -83,5 +103,14 @@ public class DocumentFriendlyUrlGenerator implements FriendlyUrlGenerator
     public String getName(  )
     {
         return GENERATOR_NAME;
+    }
+    
+    private void init()
+    {
+        String strKeyPrefix = SEODataKeys.PREFIX_GENERATOR + getClass().getName();
+        _bCanonical = DatastoreService.getDataValue( strKeyPrefix + SEODataKeys.SUFFIX_CANONICAL, DatastoreService.VALUE_TRUE).equals( DatastoreService.VALUE_TRUE);
+        _bSitemap = DatastoreService.getDataValue( strKeyPrefix + SEODataKeys.SUFFIX_SITEMAP, DatastoreService.VALUE_TRUE).equals( DatastoreService.VALUE_TRUE);
+        _strChangeFreq = DatastoreService.getDataValue( strKeyPrefix + SEODataKeys.SUFFIX_CHANGE_FREQ, DEFAULT_CHANGE_FREQ );
+        _strPriority = DatastoreService.getDataValue( strKeyPrefix + SEODataKeys.SUFFIX_PRIORITY, DEFAULT_PRIORITY );
     }
 }
